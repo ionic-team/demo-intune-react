8d6ac6843b67e3a8d725a738e568d3247c0cd138
Max Lynch <max@ionic.io>
2021-09-17 13:25:37 -0500

    Small updates

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,16 @@
-import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useCallback, useEffect, useState } from 'react';
-import './Home.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { useCallback, useEffect, useState } from "react";
+import "./Home.css";
 
-import IntuneMAM from '../IntuneMAM';
-import { useHistory } from 'react-router';
+import IntuneMAM from "../IntuneMAM";
+import { useHistory } from "react-router";
 
 const Login: React.FC = () => {
   const history = useHistory();
@@ -18,27 +25,26 @@ const Login: React.FC = () => {
   });
 
   const login = useCallback(async () => {
-    /*
     var authInfo = await (IntuneMAM as any).acquireToken({
-      scopes: ["https://graph.microsoft.com/.default"]
+      scopes: ["https://graph.microsoft.com/.default"],
     });
 
-    console.log('Got auth info', authInfo);
+    console.log("Got auth info", authInfo);
 
     await (IntuneMAM as any).registerAndEnrollAccount({
-      upn: authInfo.upn
+      upn: authInfo.upn,
     });
-    */
-   await IntuneMAM.loginAndEnrollAccount();
+
+    // await IntuneMAM.loginAndEnrollAccount();
 
     const user = await IntuneMAM.enrolledAccount();
 
     if (user.upn) {
-      console.log('Got user, going home');
-      setTimeout(() => history.replace('/home'), 500);
+      console.log("Got user, going home");
+      setTimeout(() => history.replace("/home"), 500);
     } else {
-      console.log('No user, logging in');
-      setTimeout(() => history.replace('/login'), 500);
+      console.log("No user, logging in");
+      setTimeout(() => history.replace("/login"), 500);
     }
   }, []);
 
